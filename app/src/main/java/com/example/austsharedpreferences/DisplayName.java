a4cc1e82a0d9fa248823784f45d53c59935b4a47
Joe Al Sokhen <jya20008@students.aust.edu.lb>
2024-03-19 19:01:37 +0200

    austSharedPreferences: basic functionality experiment
    Signed-off-by: Joe Al Sokhen <jya20008@students.aust.edu.lb>

--- app/src/main/java/com/example/austsharedpreferences/DisplayName.java
+++ app/src/main/java/com/example/austsharedpreferences/DisplayName.java
@@ -1,11 +1,9 @@
 package com.example.austsharedpreferences;
 
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,38 +11,28 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class DisplayName extends AppCompatActivity {
 
     SharedPreferences sp;
-    EditText contactName, major;
-
+    TextView Name;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_display_name);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        contactName = findViewById(R.id.etName);
-        major = findViewById(R.id.etMajor);
         sp = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
-    }
 
-    public void saveName(View view) {
-        String name = contactName.getText().toString().trim();
-        String s_major = major.getText().toString().trim();
-        int age = 45;
-        sp.edit().putString("NAME", name).apply();
-        sp.edit().putString("MAJOR", s_major).apply();
-        sp.edit().putString("AGE", Integer.toString(age)).apply();
-    }
+        Name = findViewById(R.id.tvName);
+        String name = sp.getString("NAME", "");
+        String age = sp.getString("AGE", "");
+        String major = sp.getString("MAJOR","");
 
-    public void goToDisplayActivity(View view) {
-        Intent i = new Intent(this, DisplayName.class);
-        startActivity(i);
+        Name.setText("Hello " + name + "\nAge: "+ age + "\nMajor: " + major);
     }
 }
